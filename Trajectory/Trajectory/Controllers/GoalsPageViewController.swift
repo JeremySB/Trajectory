3cfe5af2b22794d7ae2c72e288117da2945c246a
Unknown <biomyth1@gmail.com>
2018-01-25 11:46:15 -0500

    Swipe Crap for goals screen

---
+++ Trajectory/Trajectory/Controllers/GoalsPageViewController.swift
@@ -0,0 +1,79 @@
+//
+//  GoalsPageViewController.swift
+//  Trajectory
+//
+//  Created by student on 1/23/18.
+//  Copyright © 2018 Recreational Hazard. All rights reserved.
+//
+
+// Tutorial used for this class
+
+//https://spin.atomicobject.com/2015/12/23/swift-uipageviewcontroller-tutorial/
+
+import UIKit
+import Foundation
+
+class GoalsPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController(storyboardID: "GoalsList"),
+                self.newViewController(storyboardID: "History")]
+    }()
+    
+    private func newViewController(storyboardID: String) -> UIViewController {
+        
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(storyboardID)")//ViewController")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+}
