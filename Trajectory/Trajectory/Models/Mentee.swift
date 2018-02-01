248831dcf93849a26180453344c04b4b9aeac42b
Jeremy Bost <bostjs1@gcc.edu>
2018-02-01 12:07:52 -0500

    Mentee and MenteeRequest models

---
+++ Trajectory/Trajectory/Models/Mentee.swift
@@ -0,0 +1,9 @@
+//
+//  Mentee.swift
+//  Trajectory
+//
+//  Created by Jeremy Bost on 2/1/18.
+//  Copyright © 2018 Recreational Hazard. All rights reserved.
+//
+
+import Foundation
---
+++ Trajectory/Trajectory/Models/MenteeRequest.swift
@@ -0,0 +1,9 @@
+//
+//  MenteeRequest.swift
+//  Trajectory
+//
+//  Created by Jeremy Bost on 2/1/18.
+//  Copyright © 2018 Recreational Hazard. All rights reserved.
+//
+
+import Foundation
