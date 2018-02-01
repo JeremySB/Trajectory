41423f636882c9f77855db37aadf43edbe8379a8
Andrew Vogel <VogelAR1@gcc.edu>
2018-02-01 08:26:12 -0500

    Potential fix for back chevron on My Organizations screen

--- Trajectory/Trajectory/Controllers/Settings/MyOrganizationsTableViewController.swift
+++ Trajectory/Trajectory/Controllers/Settings/MyOrganizationsTableViewController.swift
@@ -61,7 +61,10 @@ class MyOrganizationsTableViewController: UITableViewController {
         }
     }
     
-
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
