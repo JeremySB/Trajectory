c5457cac94ab276cf1f25ef40f6415bd4e3e7ede
Sam Casteel <biomyth1@gmail.com>
2018-02-06 12:03:34 -0500

    Goal Model Fixes

--- Trajectory/Trajectory/Models/Goal.swift
+++ Trajectory/Trajectory/Models/Goal.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-class Goals{
-    var goalId: String?
+class Goal{
+    var goalID: String?
     var ownerID: String?
     var totalProgress: Int?
     var currentProgress: Int?
@@ -19,5 +19,18 @@ class Goals{
     var targetPerMonth: Int?
     var startDate: String?
     var endDate: String?
+    
+    init(){
+        goalID = "yesGoalID"
+        ownerID = "yesOwnerID"
+        totalProgress = 100
+        currentProgress = 50
+        title = "Yaaassss Goal"
+        targetPerDay = 2
+        targetPerWeek = 3
+        targetPerMonth = 200
+        startDate = "2/7/2018"
+        endDate = "3/1/2018"
+    }
     
 }
