430910e90b2213bce465bb25bbea1085aabfc2aa
Sam Casteel <biomyth1@gmail.com>
2018-02-06 11:23:26 -0500

    Goal Model Added

---
+++ Trajectory/Trajectory/Models/Goal.swift
@@ -0,0 +1,23 @@
+//
+//  Goal.swift
+//  Trajectory
+//
+//  Created by student on 2/6/18.
+//  Copyright © 2018 Recreational Hazard. All rights reserved.
+//
+
+import Foundation
+
+class Goals{
+    var goalId: String?
+    var ownerID: String?
+    var totalProgress: Int?
+    var currentProgress: Int?
+    var title: String?
+    var targetPerDay: Int?
+    var targetPerWeek: Int?
+    var targetPerMonth: Int?
+    var startDate: String?
+    var endDate: String?
+    
+}
