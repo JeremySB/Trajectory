fec9e609745361d2919a8d3c61c120448413f477
Jeremy Bost <bostjs1@gcc.edu>
2018-02-07 17:43:42 -0500

    Add our first unit test

---
+++ Trajectory/Trajectory/Protocols/AuthenticationService.swift
@@ -0,0 +1,11 @@
+//
+//  AuthenticationService.swift
+//  Trajectory
+//
+//  Created by Jeremy Bost on 2/7/18.
+//  Copyright © 2018 Recreational Hazard. All rights reserved.
+//
+
+protocol AuthenticationService {
+    
+}
