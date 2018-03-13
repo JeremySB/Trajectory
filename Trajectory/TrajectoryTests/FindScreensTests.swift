64fc4ea2e9bee02113a957afafe7028cacd5c440
Andrew Vogel <VogelAR1@gcc.edu>
2018-03-13 11:14:19 -0400

    Finished setup for UI and unit tests on all Find screens

---
+++ Trajectory/TrajectoryTests/FindScreensTests.swift
@@ -0,0 +1,42 @@
+//
+//  FindScreensTests.swift
+//  TrajectoryTests
+//
+//  Created by Andrew Vogel on 3/13/18.
+//  Copyright © 2018 Recreational Hazard. All rights reserved.
+//
+
+import XCTest
+
+class FindScreensTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    // --Tests for Find People View Controller--
+    func testPeopleSearchForMatches() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPeopleUpdateSearchResults() {
+        
+    }
+    
+    // --Tests for Find Organizations View Controller--
+    func testOrganizationsSearchForMatches() {
+        
+    }
+    
+    func testOrganizationsUpdateSearchResults() {
+        
+    }
+    
+}
--- Trajectory/TrajectoryUITests/FindUITests.swift
+++ Trajectory/TrajectoryUITests/FindUITests.swift
@@ -38,29 +38,72 @@ class FindUITests: XCTestCase {
         app.terminate()
     }
     
-    func testPeopleSearchBar() {
+    // --People Find Screen Tests--
+    func testPeopleSearchBarValidText() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         app.tabBars.buttons["Find"].tap()
         let searchForPeopleSearchField = app.searchFields["Search for people"]
         searchForPeopleSearchField.tap()
-        app/*@START_MENU_TOKEN@*/.keys["A"]/*[[".keyboards.keys[\"A\"]",".keys[\"A\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        searchForPeopleSearchField.typeText("A")
-        app/*@START_MENU_TOKEN@*/.keys["n"]/*[[".keyboards.keys[\"n\"]",".keys[\"n\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        searchForPeopleSearchField.typeText("n")
-        app/*@START_MENU_TOKEN@*/.keys["d"]/*[[".keyboards.keys[\"d\"]",".keys[\"d\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        searchForPeopleSearchField.typeText("d")
-        app/*@START_MENU_TOKEN@*/.keys["r"]/*[[".keyboards.keys[\"r\"]",".keys[\"r\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        searchForPeopleSearchField.typeText("r")
-        app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        searchForPeopleSearchField.typeText("e")
-        app/*@START_MENU_TOKEN@*/.keys["w"]/*[[".keyboards.keys[\"w\"]",".keys[\"w\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        searchForPeopleSearchField.typeText("w")
-       /* app.collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.containing(.image, identifier:"profileImg").element.tap()
+        searchForPeopleSearchField.typeText("andrew")
+        /*app.collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.containing(.image, identifier:"profileImg").element.tap()
         app.buttons["Contact Mentor"].tap()
         app.sheets["Please confirm that you want to send this mentee request"].buttons["Confirm"].tap()
         */
     }
     
+    func testPeopleSearchBarInvalidText() {
+        
+    }
+    
+    func testPeopleSearchBarNoText() {
+        
+    }
+    
+    func testPeopleSearchBarCancelButton() {
+        
+    }
+    
+    func testPeopleSearchBarSearchButton() {
+        
+    }
+    
+    func testPeopleSwipeToLeft() {
+        
+    }
+    
+    // --Organization Find Screen Tests--
+    func testOrganizationSearchBarValidText() {
+        
+    }
+    
+    func testOrganizationSearchBarInvalidText() {
+        
+    }
+    
+    func testOrganizationSearchBarNoText() {
+        
+    }
+    
+    func testOrganizationSearchBarCancelButton() {
+        
+    }
+    
+    func testOrganizationSearchBarSearchButton() {
+        
+    }
+    
+    func testOrganizationSwipeToRight() {
+        
+    }
+    
+    // --Mentor Information Screen Tests--
+    func testContactMentorConfirmButton() {
+        
+    }
+    
+    func testContactMentorCancelButton() {
+        
+    }
 }
