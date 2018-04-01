986a050a39616d1e5a8efe5433a5f99c52efae73
Andrew Vogel <VogelAR1@gcc.edu>
2018-04-01 17:47:23 -0400

    Checkin buttons and functionality setup further

--- Trajectory/Trajectory/Controllers/Community/MyMentorCheckinViewController.swift
+++ Trajectory/Trajectory/Controllers/Community/MyMentorCheckinViewController.swift
@@ -15,6 +15,9 @@ class MyMentorCheckinViewController: UIViewController, UserChild {
         get{return self._user}
         set{ mentorName?.text = newValue.name; self._user = newValue; viewDidAppear(false)}
     }
+    
+    //Variable that holds current check-in
+    var currentCheckIn : String? = nil
 
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var mentorName: UILabel!
@@ -31,6 +34,7 @@ class MyMentorCheckinViewController: UIViewController, UserChild {
             imageService.bindProfileImage(for: uid, to: profileImage)
         }
         // Do any additional setup after loading the view.
+        getAndSetCheckInButtons()
     }
     
     @IBAction func sendToCalendar(_ sender: Any) {
@@ -39,27 +43,74 @@ class MyMentorCheckinViewController: UIViewController, UserChild {
         }
     }
     
-    //Function that checks latest check-ins and enables/disables buttons appropriately
-    func setCheckInButtons() {
+    //Function that gets latest check-in and enables current check-in if applicable
+    func getAndSetCheckInButtons() {
+        //TODO: pull latest check-in from backend and set it as current check-in if date stamp matches today's date
         
+        //Set check-in buttons corresponding to value of current check-in
+        if currentCheckIn == "doingWell" {
+            doingWellButton.setTitle("Check-In Confirmed", for: .normal)
+            //TODO: figure out color //doingWellButton.backgroundColor = UIColor.green
+            doingWellButton.isEnabled = false
+        } else if currentCheckIn == "alright" {
+            alrightButton.setTitle("Check-In Confirmed", for: .normal)
+            //TODO: figure out color //alrightButton.backgroundColor = UIColor.yellow
+            alrightButton.isEnabled = false
+        } else if currentCheckIn == "notGood" {
+            notGoodButton.setTitle("Check-In Confirmed", for: .normal)
+            //TODO: figure out color //notGoodButton.backgroundColor = UIColor.red
+            notGoodButton.isEnabled = false
+        }
     }
 
     @IBAction func doingWellCheckIn(_ sender: Any) {
-        let dateStamp = Date()
-        print(dateStamp)
-        //TODO: save status to backend with a date stamp and send notification to mentor
+        if currentCheckIn != "doingWell" {
+            doingWellButton.setTitle("Check-In Confirmed", for: .normal)
+            //TODO: figure out color //doingWellButton.backgroundColor = UIColor.green
+            
+            //Enable/disable buttons appropriately
+            doingWellButton.isEnabled = false
+            alrightButton.isEnabled = true
+            notGoodButton.isEnabled = true
+            
+            //TODO: save status to backend with a date stamp and send notification to mentor
+            let dateStamp = Date()
+            print(dateStamp)
+            
+        }
     }
     
     @IBAction func alrightCheckIn(_ sender: Any) {
-        let dateStamp = Date()
-        print(dateStamp)
-        //TODO: save status to backend with a date stamp and send notification to mentor
+        if currentCheckIn != "alright" {
+            alrightButton.setTitle("Check-In Confirmed", for: .normal)
+            //TODO: figure out color //alrightButton.backgroundColor = UIColor.yellow
+            
+            //Enable/disable buttons appropriately
+            doingWellButton.isEnabled = true
+            alrightButton.isEnabled = false
+            notGoodButton.isEnabled = true
+            
+            //TODO: save status to backend with a date stamp and send notification to mentor
+            let dateStamp = Date()
+            print(dateStamp)
+            
+        }
     }
     
     @IBAction func notGoodCheckIn(_ sender: Any) {
-        let dateStamp = Date()
-        print(dateStamp)
-        //TODO: save status to backend with a date stamp and send notification to mentor
+        if currentCheckIn != "notGood" {
+            notGoodButton.setTitle("Check-In Confirmed", for: .normal)
+            //TODO: figure out color //notGoodButton.backgroundColor = UIColor.red
+            
+            //Enable/disable buttons appropriately
+            doingWellButton.isEnabled = true
+            alrightButton.isEnabled = true
+            notGoodButton.isEnabled = false
+            
+            //TODO: save status to backend with a date stamp and send notification to mentor
+            let dateStamp = Date()
+            print(dateStamp)
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
