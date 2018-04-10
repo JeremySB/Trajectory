e1b2ed5b5d5ccff772561d66fc1f442211dd0733
Andrew Vogel <VogelAR1@gcc.edu>
2018-04-10 12:05:57 -0400

    New update end date screen created

---
+++ Trajectory/Trajectory/Controllers/Community/UpdateEndDateViewController.swift
@@ -0,0 +1,50 @@
+//
+//  UpdateEndDateViewController.swift
+//  Trajectory
+//
+//  Created by Andrew Vogel on 4/10/18.
+//  Copyright © 2018 Recreational Hazard. All rights reserved.
+//
+
+import UIKit
+
+class UpdateEndDateViewController: UIViewController {
+    
+    
+    @IBOutlet weak var endDate: UIDatePicker!
+    
+    @IBOutlet weak var acceptBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let offSetDays = 90
+        let defaultDate = Date().addingTimeInterval(TimeInterval(60 * 60 * 24 * offSetDays))
+        endDate.minimumDate = Date()
+        endDate.setDate(defaultDate, animated: true)
+        endDate.setValue(UIColor.white, forKey: "textColor")
+
+        acceptBtn.layer.borderColor = UIColor.lightGray.cgColor
+        acceptBtn.layer.borderWidth = 2
+        acceptBtn.layer.cornerRadius = 5
+        
+    }
+    
+
+   
+    @IBAction func setDateAndAcceptBtn(_ sender: Any) {
+        //TODO: update end date on backend
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
