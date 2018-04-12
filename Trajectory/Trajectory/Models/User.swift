452c14872c4f41f55046501e1e7e7b45458400e9
Andrew Vogel <VogelAR1@gcc.edu>
2018-04-12 12:26:25 -0400

    Add field to user

--- Trajectory/Trajectory/Models/User.swift
+++ Trajectory/Trajectory/Models/User.swift
@@ -18,12 +18,13 @@ class User: Codable {
     var emailAddress: String?
     var notificationTokens: [String: Bool]?
     var willingToMentor: Bool?
+    var lastOrganizationName: String?
     
     var id: String? = nil
     // set to true if user doc does not exist on server, but created locally
     var isNew: Bool = false
     
     enum CodingKeys: String, CodingKey {
-        case name, hobbies, professionalInterests, denomination, objectives, phoneNumber, emailAddress, notificationTokens, willingToMentor
+        case name, hobbies, professionalInterests, denomination, objectives, phoneNumber, emailAddress, notificationTokens, willingToMentor, lastOrganizationName
     }
 }
