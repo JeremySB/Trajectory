5f30d16d96d2730be7cc1f98aa04166cd8d1699c
Jeremy Bost <bostjs1@gcc.edu>
2018-04-16 17:57:48 -0400

    Fixed a failing ui test

--- Trajectory/TrajectoryUITests/RegistrationTests.swift
+++ Trajectory/TrajectoryUITests/RegistrationTests.swift
@@ -28,57 +28,57 @@ class RegistrationTests: XCTestCase {
         app.terminate()
     }
     
+    func testtest() {
+        
+        
+        
+        
+        
+    }
+    
     func testEmailRegistration() {
         let num = Int(arc4random_uniform(500000)) + 1000
         let email: String = "uitestnumber\(num)@gettrajectory.com"
         
-        let emailTextField = app.textFields["Email Address"]
+        let emailTextField = app.textFields["emailField"]
         emailTextField.tap()
         emailTextField.typeText(email)
-        app.buttons["Continue"].tap()
+        app.buttons["continueButton"].tap()
         
-        let passwordSecureTextField = app.secureTextFields["Password"]
+        let passwordSecureTextField = app.secureTextFields["passwordField"]
         passwordSecureTextField.tap()
         passwordSecureTextField.typeText("asdlkfjsdlkfj")
-        app.otherElements.containing(.navigationBar, identifier:"Create A Password").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.tap()
-        app.buttons["Create Account"].tap()
+        app.buttons["createAccountButton"].tap()
         
-        let pleaseEnterYourNameTextField = app.textFields["  Name"]
+        let pleaseEnterYourNameTextField = app.textFields["nameField"]
         pleaseEnterYourNameTextField.tap()
         pleaseEnterYourNameTextField.typeText("name name")
         
-        let element = app.otherElements.containing(.navigationBar, identifier:"Profile Setup").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
-        element.tap()
-        
-        let nextButton = app.buttons["Next"]
-        nextButton.tap()
+        app.buttons["nextButton"].tap()
         
         let phonenumberTextField = app/*@START_MENU_TOKEN@*/.textFields["phoneNumber"]/*[[".textFields[\"Phone Number\"]",".textFields[\"phoneNumber\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         phonenumberTextField.tap()
         phonenumberTextField.typeText("3333333333")
-        element.tap()
-        nextButton.tap()
+        app.buttons["nextButton"].tap()
         
-        let element2 = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
-        let textView = element2.children(matching: .textView).element(boundBy: 0)
-        textView.tap()
-        textView.typeText("Hobbies")
+        let hobbiesField = app.textViews["hobbiesField"]
+        hobbiesField.tap()
+        hobbiesField.typeText("Hobbies")
         
-        let textView2 = element2.children(matching: .textView).element(boundBy: 1)
-        textView2.tap()
+        let textView2 = app.textViews["interestsField"]
         textView2.tap()
         textView2.typeText("Interests")
-        nextButton.tap()
+        app.buttons["nextButton"].tap()
         
-        let textField = element2.children(matching: .textField).element
+        
+        let textField = app.textFields["denomField"]
         textField.tap()
-        textField.typeText("  Denomination")
+        textField.typeText("Denomination")
         
-        let textView3 = element2.children(matching: .textView).element
-        textView3.tap()
+        let textView3 = app.textViews["objectivesField"]
         textView3.tap()
         textView3.typeText("Goals")
-        app.buttons["Finish"].tap()
+        app.buttons["finishButton"].tap()
         
         
 
